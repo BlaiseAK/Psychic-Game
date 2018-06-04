355dfb1be401eec7aa4ba0bea0ef40ff335cf170
BlaiseAK <blaisekress@yahoo.com>
2018-06-04 16:29:56 -0400

    Added image and css to document and corrected some bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //pick a random letter
 
-var letArr = ["a", "b", "c", "d", "e", "f", "g", "h", "I", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var letArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
 
